184b9212bfc1cff2acb56ff4c0ebf314fe31f9d1
Ethan Glasser-Camp <ethan@betacantrips.com>
2014-12-05 14:45:39 -0500

    define EurekaConnection
    These seem like useful things to keep around.

--- src/Network/Eureka.hs
+++ src/Network/Eureka.hs
@@ -1,15 +1,17 @@
+{-# LANGUAGE NamedFieldPuns #-}
 module Network.Eureka (withEureka, EurekaConfig(..), InstanceConfig(..),
                        EurekaConnection) where
 
 import Data.Map (Map)
+import Control.Concurrent (ThreadId, forkIO, threadDelay)
 import Control.Exception (bracket)
 
-data EurekaConfig = EurekaConfiguration {
+data EurekaConfig = EurekaConfig {
       eurekaServerServiceUrls :: Map String [String]
       -- ^ The URLs for Eureka, per availability zone.
     , eurekaInstanceInfoReplicationInterval :: Int
       -- ^ How often, in seconds, to push instance info to Eureka.
-    }
+    } deriving Show
 
 data InstanceConfig = InstanceConfig {
       instanceServiceUrlDefault :: String
@@ -37,9 +39,19 @@ data InstanceConfig = InstanceConfig {
       -- ^ Whether the instance should be marked as "up" immediately.  Some
       -- services might not be ready at startup, in which case this should be
       -- false.
-    }
+    } deriving Show
 
-data EurekaConnection = EurekaConnection
+data EurekaConnection = EurekaConnection {
+      eConnEurekaConfig :: EurekaConfig
+      -- ^ The configuration specifying where Eureka is.
+    , eConnInstanceConfig :: InstanceConfig
+      -- ^ The configuration about this instance and how it will talk to Eureka.
+    , eConnHeartbeatThread :: ThreadId
+      -- ^ Thread that periodically posts a heartbeat to Eureka so that it knows
+      -- we're still alive.
+    , eConnInstanceInfoReplicatorThread :: ThreadId
+      -- ^ Thread that periodically pushes instance information to Eureka.
+    } deriving Show
 
 withEureka :: EurekaConfig -> InstanceConfig -> (EurekaConnection -> IO a) -> IO a
 withEureka eConfig iConfig m = bracket (connectEureka eConfig) disconnectEureka (registerAndRun m)
