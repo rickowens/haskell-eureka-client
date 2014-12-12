c52ad4e5935eb1c08ac1e81b7311a8ed58164f22
Ethan Glasser-Camp <ethan@betacantrips.com>
2014-12-12 18:53:53 -0500

    test: get server URL from command-line

--- src/test.hs
+++ src/test.hs
@@ -1,12 +1,24 @@
 import Network.Eureka (withEureka,
-    EurekaConfig(eurekaInstanceInfoReplicationInterval), defaultEurekaConfig,
+    EurekaConfig(eurekaInstanceInfoReplicationInterval, eurekaRegion,
+                 eurekaServerServiceUrls,
+                 eurekaAvailabilityZones), defaultEurekaConfig,
     InstanceConfig(instanceLeaseRenewalInterval), defaultInstanceConfig,
     defaultInstanceInfo)
 import Control.Concurrent (threadDelay)
+import System.Environment (getArgs)
+import qualified Data.Map as Map
 
 main :: IO ()
-main = withEureka myEurekaConfig myInstanceConfig defaultInstanceInfo $ \_ -> do
-    sequence_ $ replicate 20 $ threadDelay $ 1000 * 1000
+main = do
+    args <- getArgs
+    let [commandLineServer] = args
+    withEureka (myEurekaConfig commandLineServer) myInstanceConfig defaultInstanceInfo $ \_ -> do
+        sequence_ $ replicate 20 $ threadDelay $ 1000 * 1000
   where
-    myEurekaConfig = defaultEurekaConfig { eurekaInstanceInfoReplicationInterval = 1 }
+    myEurekaConfig serverUrl = defaultEurekaConfig {
+        eurekaInstanceInfoReplicationInterval = 1,
+        eurekaServerServiceUrls = Map.fromList [("us-east-1a", [serverUrl])],
+        eurekaAvailabilityZones = Map.fromList [("us-east-1", ["us-east-1a"])],
+        eurekaRegion = "us-east-1"
+        }
     myInstanceConfig = defaultInstanceConfig { instanceLeaseRenewalInterval = 1 }
