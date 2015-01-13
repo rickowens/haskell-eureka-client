3f2dc4244093c3f2ceb2940a29b361d10a9b857a
Rick Owens <rowens@sumall.com>
2015-01-12 21:35:39 -0500

    no-op - whitespace

--- src/Network/Eureka.hs
+++ src/Network/Eureka.hs
@@ -1,11 +1,17 @@
 {-# LANGUAGE NamedFieldPuns, OverloadedStrings #-}
-module Network.Eureka (withEureka, EurekaConfig(..), InstanceConfig(..),
-                       def,
-                       discoverDataCenterAmazon, setStatus,
-                       lookupByAppName,
-                       InstanceStatus(..),
-                       DataCenterInfo(DataCenterMyOwn),
-                       EurekaConnection, AvailabilityZone, Region) where
+module Network.Eureka (withEureka,
+  EurekaConfig(..),
+  InstanceConfig(..),
+  def,
+  discoverDataCenterAmazon,
+  setStatus,
+  lookupByAppName,
+  InstanceStatus(..),
+  DataCenterInfo(DataCenterMyOwn),
+  EurekaConnection,
+  AvailabilityZone,
+  Region
+) where
 
 import Control.Applicative ((<$>), (<*>))
 import Control.Concurrent (ThreadId, forkIO, killThread, threadDelay)
